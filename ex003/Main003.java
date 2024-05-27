97dafe023740c8df8ac725ebf3581306274ec291
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-05-27 16:45:35 -0300

    Done Exercise 3

---
+++ ex003/Main003.java
@@ -0,0 +1,20 @@
+//Print idade, primeira letra do nome, altura. (variaveis)
+
+public class Main003
+{
+	public static void main(String[] args) {
+	    int idade;
+	    float altura;
+	    char pLetra;
+	    
+	    
+	    idade = 18;
+	    altura = 1.86f;  // tem um f no final do numero, por motivos que eu desconheço...
+	    pLetra = 'G'; // type char utiliza aspas simples ('') e type String utiliza aspas duplas ("")
+	    
+	    System.out.println(idade);
+	    System.out.println(altura);
+	    System.out.println(pLetra);
+
+	}
+}
