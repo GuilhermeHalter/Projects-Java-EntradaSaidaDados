31ab1056e68ebcdb7efdbcfb561cd9f7361d9edc
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-05-27 16:30:07 -0300

    Done Exercise 2

---
+++ ex002/Main002.java
@@ -0,0 +1,12 @@
+// Imprimindo Nome, rua, telefone, email,cpf na tela
+
+public class Main002
+{
+	public static void main(String[] args) {
+		System.out.println("Nome: Guilherme Halter Nunes");
+		System.out.println("Rua -------");
+		System.out.println("Telefone: (00) 00000-0000");
+		System.out.println("Email: halter.guilhermenunes@gmail.com");
+		System.out.println("cpf: 000.000.000-00");
+	}
+}
