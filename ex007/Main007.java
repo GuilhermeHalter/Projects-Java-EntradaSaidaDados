99b29dfc6779c2b309212defba790d93591e6708
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-05-31 18:03:42 -0300

    Done Exercise 7

--- ex007/Main007.java
+++ ex007/Main007.java
@@ -1,1 +1,17 @@
 //transformando metros para centimetros
+
+import java.util.Scanner;
+
+public class Main007 {
+    public static void main(String[] args){
+        
+        double centimetros = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Insira o valor de metros: ");
+        double metros = scanner.nextDouble();
+
+        System.out.println("O valor de " + metros + " metros em centimetros é: " + metros * centimetros + " cm");
+    }
+}
