7bb93badfc07e39e8f9fca0bb5ca24a7c08d9f8b
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-06-04 18:58:27 -0300

    Done Exercise 9

--- ex009/Main009.java
+++ ex009/Main009.java
@@ -1,1 +1,18 @@
 //Construa um algoritmo que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+import java.util.Scanner;
+
+public class Main009
+{
+	public static void main(String[] args) {
+	    
+	    Scanner scanner = new Scanner(System.in);
+	    
+		System.out.println("Digite o valor do lado do quadrado: ");
+		double lado = scanner.nextDouble();
+		
+	    double area = lado * lado;
+		
+		System.out.println("A area de um quadrado com lado " + lado + " é de " + area + " e o dobro dessa area é de " + area * 2);
+	}
+}
