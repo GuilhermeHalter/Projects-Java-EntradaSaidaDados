aa5f8343d95f3c80dc708c895701cacbf477db3d
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-06-04 19:08:43 -0300

    Done Exercise 11

--- ex011/Main011.java
+++ ex011/Main011.java
@@ -1,1 +1,28 @@
 //Construa um algoritmo que peça as 4 notas bimestrais e mostre a média, com uma casa decimal  
+
+import java.util.Scanner;
+
+public class Main010
+{
+	public static void main(String[] args) {
+	    
+	    Scanner scanner = new Scanner(System.in);
+	    
+		System.out.println("Digite o valor da primeira nota: ");
+		double n1 = scanner.nextDouble();
+		
+		System.out.println("Digite o valor da segunda nota: ");
+		double n2 = scanner.nextDouble();
+		
+		System.out.println("Digite o valor da terceira nota: ");
+		double n3 = scanner.nextDouble();
+		
+		System.out.println("Digite o valor do quarta nota: ");
+		double n4 = scanner.nextDouble();
+		
+	    double soma = n1 + n2 + n3 + n4;
+	    double media = soma / 4;
+		
+		System.out.println("A media das notas é " + media);
+	}
+}
