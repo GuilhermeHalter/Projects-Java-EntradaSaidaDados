f1903f84faa59e0ce00e7176e15356ce0d99054b
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-06-04 19:16:20 -0300

    Done Exercise 12

--- ex012/Main012.java
+++ ex012/Main012.java
@@ -1,2 +1,22 @@
 /*Construa um algoritmo que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
 Calcule e mostre o total do seu salário no referido mês.*/
+
+import java.util.Scanner;
+
+public class Main010
+{
+	public static void main(String[] args) {
+	    
+	    Scanner scanner = new Scanner(System.in);
+	    
+		System.out.println("Quantos você ganha por hora? ");
+		double valor = scanner.nextDouble();
+		
+		System.out.println("Quantas horas voce trabalha no mes? ");
+		double horas = scanner.nextDouble();
+		
+	    double salario = valor * horas;
+		
+		System.out.println("O seu salario no mes é de " + salario);
+	}
+}
