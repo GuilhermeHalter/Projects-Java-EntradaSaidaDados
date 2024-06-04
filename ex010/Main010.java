584b7694044405fbed119853e7ce97d9a929432d
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-06-04 19:03:50 -0300

    Done Exercise 10

--- ex010/Main010.java
+++ ex010/Main010.java
@@ -1,1 +1,21 @@
 //Construa um algoritmo que peça dois números, calcule e mostre a soma dos mesmos 
+
+import java.util.Scanner;
+
+public class Main010
+{
+	public static void main(String[] args) {
+	    
+	  Scanner scanner = new Scanner(System.in);
+	    
+		System.out.println("Digite o valor do primeiro numero: ");
+		double n1 = scanner.nextDouble();
+		
+		System.out.println("Digite o valor do segundo numero: ");
+		double n2 = scanner.nextDouble();
+		
+	  double soma = n1 + n2;
+		
+		System.out.println("A soma dos numeros " + n1 +" "+ n2 + " é " + soma);
+	}
+}
