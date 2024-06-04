c8e3f1d7117585f8d9e0a0939fae8ac6bd312581
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-06-04 18:52:36 -0300

    Done Exercise 8

--- ex008/Main008.java
+++ ex008/Main008.java
@@ -1,1 +1,17 @@
 //Construa um algoritmo que peça um número com 5 casas decimais e arredonde para duas.
+
+import java.text.DecimalFormat; 
+import java.util.Scanner;
+
+public class Main008
+{
+	public static void main(String[] args) {
+	    
+	    Scanner scanner = new Scanner(System.in);
+	    
+		System.out.println("Digite um numero com 5 casas decimais: ");
+		double numero = scanner.nextDouble();
+		
+		System.out.println(new DecimalFormat("#,##0.00").format(numero));
+	}
+}
