5ff852a764f8f63078cb253fc781c3dc3a658b2e
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-07-04 22:47:12 -0300

    Done Exercise 15

--- ex015/Main015.java
+++ ex015/Main015.java
@@ -1,2 +1,23 @@
-/*⦁	Construa um algoritmo que peça uma temperatura em graus Celsius(ºC), 
-transforme e mostre na tela a temperatura em graus Farenheit(ºF).*/
+/*
+Construa um algoritmo que peça uma temperatura em graus Celsius(ºC), 
+transforme e mostre na tela a temperatura em graus Farenheit(ºF).
+*/
+
+import java.util.Scanner;
+
+public class Main015
+{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite a temperatura em graus Celsius(ºC)");
+        double Celsius = scanner.nextDouble();
+        
+        double Farenheit;
+        
+        Farenheit = (Celsius * 1.8) + 32; 
+        
+        System.out.println("A temperatura de " + Celsius + " Celsius equivale a " + Farenheit + " Farenheit");
+        
+    }
+}
