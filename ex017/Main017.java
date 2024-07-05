d0602bca42a1cf1abc4f94e45c12f2276cfbb6a4
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-07-05 12:58:48 -0300

    Done Exercise 17

--- ex017/Main017.java
+++ ex017/Main017.java
@@ -5,3 +5,43 @@ Uma determinada empresa vende uma máquina que possui seis motores. Um de 20 CV,
 ⦁	Calcule e apresente na tela a quantidade de motores que deverão ser comprados de cada modelo; 
 ⦁	Calcule e apresente na tela o custo total por máquina, o custo por tipo de motor e o custo total da compra.
 */
+
+import java.util.Scanner;
+
+public class Main017
+{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int motorVinte, motorUm, motorCinco;
+        
+        float valorMaquina, valorMotorVinte, valorMotorUm, valorMotorCinco, valorTotal;
+        
+        System.out.println("Informe a quantidade de maquinas a serem compradas:  ");
+        int numMaquina = scanner.nextInt();
+        
+        valorMaquina = (1500 + (600*3) + (300*2)); 
+        
+        motorVinte = 1 * numMaquina;
+        motorUm    = 2 * numMaquina;
+        motorCinco = 3 * numMaquina;
+        
+        valorMotorVinte = motorVinte * 1500;
+        valorMotorUm = motorUm * 300;
+        valorMotorCinco = motorCinco * 600;
+        
+        valorTotal = valorMotorVinte + valorMotorUm + valorMotorCinco;
+        
+        System.out.println("O numero de motores 20CV que deverao ser comprados são de: " + motorVinte);
+        System.out.println("O numero de motores 1CV que deverao ser comprados são de: " + motorUm);
+        System.out.println("O numero de motores 5CV que deverao ser comprados são de: " + motorCinco);
+
+        System.out.println("O valor a pagar de 1 maquina eh de "+ valorMaquina);
+        
+        System.out.println("O valor a pagar nos motores 20CV é de " + valorMotorVinte);
+        System.out.println("O valor a pagar nos motores 1CV é de " + valorMotorUm);
+        System.out.println("O valor a pagar nos motores 5CV é de " + valorMotorCinco);
+        
+        System.out.println("O valor total a pagar eh de " + valorTotal);
+    }
+}
