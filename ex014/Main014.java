e20439530ff20afece944784119eaf3c2fd20d1f
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-07-04 22:43:42 -0300

    Done Exercise 14

--- ex014/Main014.java
+++ ex014/Main014.java
@@ -1,2 +1,21 @@
-/*Construa um algoritmo que peça a temperatura em graus Farenheit(ºF)
-, transforme e mostre na tela a temperatura em graus Celsius(ºC). C=5*(F-32)/9}.*/
+/*
+Construa um algoritmo que peça a temperatura em graus Farenheit(ºF)
+, transforme e mostre na tela a temperatura em graus Celsius(ºC). C=5*(F-32)/9}.
+*/
+import java.util.Scanner;
+
+public class Main014
+{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite a temperatura em graus Farenheit(ºF)");
+        double Farenheit = scanner.nextDouble();
+        
+        double Celsius;
+        
+        Celsius = 5*(Farenheit - 32)/9;
+        
+        System.out.println("A temperatura de " + Farenheit + " Farenheit equivale a " + Celsius + " Celsius");
+    }
+}
